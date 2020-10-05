5c427a0d7de2597e9aa8df3b5886cf1408ac5164
yanuk <72168260+yanuk@users.noreply.github.com>
2020-10-05 21:34:15 +0100

    Update Program.cs

--- rectangle/Program.cs
+++ rectangle/Program.cs
@@ -4,13 +4,61 @@ namespace rectangle
 {
     class Program
     {
+           static int checkIfNumber(string str)
+        //Проверка введено число или текст. 
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]) == false)
+                {
+                    //введен текст
+                    return 0;
+                }
+            }
+            //введено число
+            return 1;
+        }
+
+
+
+
         static void Main(string[] args)
         {
-
+            int width = 0;
+            int flag = 0;
+        
             Console.WriteLine("Введите ширину");
-            int width = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите высота");
-            int high = int.Parse(Console.ReadLine());       
+
+            while (flag == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите ширину");
+                string str = Console.ReadLine();
+                flag = checkIfNumber(str);
+
+                if (flag == 1)
+                {
+                    width = int.Parse(str);
+                    break;
+                }
+            }
+            int high = 0;
+            flag = 0;
+            while (flag == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите высоту");
+                string str = Console.ReadLine();
+                flag = checkIfNumber(str);
+
+                
+                if (flag == 1)
+                {
+                   high = int.Parse(str);
+                    break;
+                }
+
+            }
 
             Console.Clear ();
             
@@ -22,8 +70,6 @@ namespace rectangle
                     Console.WriteLine("*");
                }
             }
-
-            
         }
     }
 }
