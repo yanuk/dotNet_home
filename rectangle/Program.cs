8f8403fa6918b7685ddbee8731b8e6662439ea4f
yanuk <72168260+yanuk@users.noreply.github.com>
2020-10-05 23:29:52 +0100

    Update Program.cs

--- rectangle/Program.cs
+++ rectangle/Program.cs
@@ -4,7 +4,7 @@ namespace rectangle
 {
     class Program
     {
-           static int checkIfNumber(string str)
+        static int checkIfNumber(string str)
         //Проверка введено число или текст. 
         {
             for (int i = 0; i < str.Length; i++)
@@ -26,14 +26,12 @@ namespace rectangle
         {
             int width = 0;
             int flag = 0;
-        
-            Console.WriteLine("Введите ширину");
 
-            while (flag == 0)
+           while (flag == 0)
             {
                 Console.Clear();
                 Console.WriteLine("Введите ширину");
-                string str = Console.ReadLine();
+                string str = Console.ReadLine();     // Проверка, введено ли число, а не буквы.
                 flag = checkIfNumber(str);
 
                 if (flag == 1)
@@ -49,27 +47,77 @@ namespace rectangle
                 Console.Clear();
                 Console.WriteLine("Введите высоту");
                 string str = Console.ReadLine();
-                flag = checkIfNumber(str);
+                flag = checkIfNumber(str);             // Проверка, введено ли число, а не буквы.
 
-                
+
                 if (flag == 1)
                 {
-                   high = int.Parse(str);
+                    high = int.Parse(str);
                     break;
                 }
 
             }
 
-            Console.Clear ();
-            
-            for (int i = 0; i < width; i++)
+            Console.Clear();
+
+            int startX = 0;
+            int startY = 0;
+
+
+            for (; ; )
+
             {
-               for (int y=0; y < high; y++)
-             {
-                    Console.SetCursorPosition(i, y);
-                    Console.WriteLine("*");
-               }
-            }
+                int yMax = Console.WindowHeight - high;
+                int xMax = Console.WindowWidth - width;
+
+                ConsoleKey keyPressed = Console.ReadKey(true).Key;
+
+                if (keyPressed == ConsoleKey.LeftArrow)
+                {
+                    startX--;
+                    if (startX <0 ) startX=0;
+                }
+
+                if (keyPressed == ConsoleKey.RightArrow)
+                {
+                    startX++;
+
+                    if (startX > xMax) startX--;
+                }
+                
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    startY--;
+                    if (startY < 0) startY = 0;
+
+                }
+
+                if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    startY++;
+                    if (startY > yMax) startY=yMax;
+                }
+
+
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                Console.Clear();
+
+                for (int i = 0;i < width; i++)
+                {
+                    for (int y = 0; y < high; y++)
+                    {
+                        Console.SetCursorPosition(i + startX, y + startY);
+                        Console.WriteLine("*");
+                        Console.SetCursorPosition(0,0);
+
+                    }
+                }
+             }
         }
+
     }
 }
